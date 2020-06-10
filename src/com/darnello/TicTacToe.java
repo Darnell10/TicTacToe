27efb44c65c6791b5e0e8d87b760bcb5281b2196
Darnell10 <DarnellOtto@ac.c4q.nyc>
2020-06-09 22:53:53 -0400

    creat function to print board or tictactoe adding switch statment to help player chose a position on the board

--- src/com/darnello/TicTacToe.java
+++ src/com/darnello/TicTacToe.java
@@ -1,9 +1,66 @@
 package com.darnello;
 
+import java.util.Scanner;
+
 public class TicTacToe {
+
+    static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
 
+        char[][] gameBoard = {{' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '},
+                {'-', '+', '-', '+', '-'},
+                {' ', '|', ' ', '|', ' '}};
 
+        printBoard(gameBoard);
+
+        System.out.println("Your turn, choose boxes 1-9.");
+        int position = scanner.nextInt();
+
+        System.out.println(position);
+
+//        switch (position){
+//            case 1:
+//                gameBoard[0][0] = 'x';
+//                break;
+//            case 2:
+//                gameBoard[0][0] = 'x';
+//                break;
+//            case 3:
+//                gameBoard[0][0] = 'x';
+//                break;
+//            case 4:
+//                gameBoard[0][0] = 'x';
+//                break;
+//            case 5:
+//                gameBoard[0][0] = 'x';
+//                break;
+//            case 6:
+//                gameBoard[0][0] = 'x';
+//                break;
+//            case 7:
+//                gameBoard[0][0] = 'x';
+//                break;
+//            case 8:
+//                gameBoard[0][0] = 'x';
+//                break;
+//            case 9:
+//                gameBoard[0][0] = 'x';
+//                break;
+//
+//        }
+
+    }
+
+    public static void printBoard(char[][] gameBoard) {
+
+        for (char[] row : gameBoard) {
+            for (char character : row) {
+                System.out.print(character);
+            }
+            System.out.println();
+        }
     }
 }
