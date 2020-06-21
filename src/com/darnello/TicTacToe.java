ff8d7ecf6a14350a8c86dd52e82e4f9a8c2b4d68
Darnell10 <DarnellOtto@ac.c4q.nyc>
2020-06-21 12:46:40 -0400

    Create peicePlacer method allowing the human playr to chose where to place their 'X'. Also added logic for computer to randomly choose its position as well

--- src/com/darnello/TicTacToe.java
+++ src/com/darnello/TicTacToe.java
@@ -1,10 +1,12 @@
 package com.darnello;
 
+import java.util.Random;
 import java.util.Scanner;
 
 public class TicTacToe {
 
     static Scanner scanner = new Scanner(System.in);
+    static Random random = new Random();
 
     public static void main(String[] args) {
 
@@ -14,44 +16,19 @@ public class TicTacToe {
                 {'-', '+', '-', '+', '-'},
                 {' ', '|', ' ', '|', ' '}};
 
-        printBoard(gameBoard);
 
         System.out.println("Your turn, choose boxes 1-9.");
         int position = scanner.nextInt();
 
         System.out.println(position);
 
-//        switch (position){
-//            case 1:
-//                gameBoard[0][0] = 'x';
-//                break;
-//            case 2:
-//                gameBoard[0][0] = 'x';
-//                break;
-//            case 3:
-//                gameBoard[0][0] = 'x';
-//                break;
-//            case 4:
-//                gameBoard[0][0] = 'x';
-//                break;
-//            case 5:
-//                gameBoard[0][0] = 'x';
-//                break;
-//            case 6:
-//                gameBoard[0][0] = 'x';
-//                break;
-//            case 7:
-//                gameBoard[0][0] = 'x';
-//                break;
-//            case 8:
-//                gameBoard[0][0] = 'x';
-//                break;
-//            case 9:
-//                gameBoard[0][0] = 'x';
-//                break;
-//
-//        }
+        piecePlacer(gameBoard, position, "player");
 
+        int computerPosition = random.nextInt(9) + 1;
+        piecePlacer(gameBoard, computerPosition, "computer");
+
+
+        printBoard(gameBoard);
     }
 
     public static void printBoard(char[][] gameBoard) {
@@ -61,6 +38,50 @@ public class TicTacToe {
                 System.out.print(character);
             }
             System.out.println();
+        }
+    }
+
+    public static void piecePlacer(char[][] gameBoard, int position, String player) {
+
+        char character = ' ';
+
+        if (player.equals("player")) {
+            character = 'X';
+        } else if (player.equals("computer")) {
+            character = '0';
+
+        }
+        switch (position) {
+            case 1:
+                gameBoard[0][0] = character;
+                break;
+            case 2:
+                gameBoard[0][2] = character;
+                break;
+            case 3:
+                gameBoard[0][4] = character;
+                break;
+            case 4:
+                gameBoard[2][0] = character;
+                break;
+            case 5:
+                gameBoard[2][2] = character;
+                break;
+            case 6:
+                gameBoard[2][4] = character;
+                break;
+            case 7:
+                gameBoard[4][0] = character;
+                break;
+            case 8:
+                gameBoard[4][2] = character;
+                break;
+            case 9:
+                gameBoard[4][4] = character;
+                break;
+            default:
+                break;
+
         }
     }
 }
